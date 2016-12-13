69b56edad4b217f9b2645154c75c6f7ab843d42a
Chris P <flippedbauxite@gmail.com>
2016-12-12 21:05:33 -0500

    Added threading

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A simple stress test written in Go. Just another way to compare your computer to everyone else's.
 
+Currently, the program creates four threads to compute step counts for values, and then computes them.
+
 #TODO:
 
-Add real functionality. ;)
+Make the program spin up new threads whenever it can, to accept the next value to be computed.
--- gollatz.go
+++ gollatz.go
@@ -9,51 +9,48 @@ import (
 )
 
 func main() {
-	// This map will store all of the numbers and how many steps it takes to complete their Collatz run.
-	var scores map[uint64]uint
-	scores = make(map[uint64]uint)
-
 	// Check if the user actually provided an argument
 	if len(os.Args) != 2 {
 		fmt.Printf("Usage: collatz [unsigned 64-bit integer]\n")
 	} else {
 		// Parse the command line argument
 		max, err := strconv.ParseUint(os.Args[1], 10, 64)
+		bufferSize := 1000
 
 		// Make sure the user wasn't stupid.
 		if checkErr(err) {
 			fmt.Printf("You dun goofed. You probably put in a non-integer value.")
 		} else {
 			// This is so we can time the program. We only want to time the main logic.
-			// Using defer will execute the function once all the logic is finished.
-			// However, this also tracks the Printf output.
-			// For now, trackTime will return a string instead of printing.
-			//defer trackTime(time.Now())
 			start := time.Now()
 
-			// This is the main loop of the program.
-			var value uint64
-			for value = 1; value < max; value++ {
-				scores[value] = collatz(value)
-			} // End of main loop
-			trackTime("The Collatz portion", start)
+			// This channel will store the results from the individual goroutines,
+			// and will be processed by the scoreboard function
+			results := make(chan Result, bufferSize)
 
-			// This is where we figure out which one took the most steps.
-			var highestScore uint
-			var highestIndex uint64
+			// This Result is going to store our highest score
+			var overallHighScore Result
 
-			// Reuse value because, hey, free memory.
-			for value = 0; value < max; value++ {
-				if scores[value] > highestScore {
-					highestScore = scores[value]
-					highestIndex = value
-				}
+			// This is where we need to start our goroutines
+			go threaded(1, max/4, results)
+			go threaded(max/4, max/2, results)
+			go threaded(max/2, (max/4)*3, results)
+			go threaded((max/4)*3, max, results)
+			for true {
+				scoreboard(results, overallHighScore)
+				fmt.Printf("The buffer is currently %d / %d full", len(results), bufferSize)
 			}
-			trackTime("The program", start)
-			fmt.Printf("%d has takes the most steps at %d.\n", highestIndex, highestScore)
-		} // End of program
+			trackTime("The Collatz portion", start)
+			fmt.Printf("%d has takes the most steps at %d.\n", overallHighScore.highestValue, overallHighScore.highestScore)
+		} // End of main program logic
 
-	}
+	} // End of program
+}
+
+// Result will store the highest scores and its corresponding value of n
+type Result struct {
+	highestScore uint
+	highestValue uint64
 }
 
 // Classic 3n+1 conjecture.
@@ -68,6 +65,34 @@ func collatz(num uint64) uint {
 		count++
 	}
 	return count
+}
+
+// This function will handle the main logic of theprogram. It will call the collatz function for a range of integers
+// It will keep a local copy of the highest score and the corresponding integer. WHenever it updates this number, it will
+// push that number off to the results channel
+func threaded(start uint64, end uint64, results chan Result) {
+	// This is where we figure out which one took the most steps.
+	var score uint
+	var value uint64
+	var highScore Result
+
+	for value = start; value < end; value++ {
+		score = collatz(value)
+		if score > highScore.highestScore {
+			highScore.highestScore = score
+			highScore.highestValue = value
+			results <- highScore
+		}
+	}
+}
+
+// This function keeps track of the actual highest score, and then reports it as it is updated.
+func scoreboard(results chan Result, highScore Result) {
+	var nextScore = <-results
+	if nextScore.highestScore > highScore.highestScore {
+		highScore = nextScore
+		fmt.Printf("%d currently takes the most steps at %d\n\n", highScore.highestValue, highScore.highestScore)
+	}
 }
 
 // This function tracks time. Yay!
